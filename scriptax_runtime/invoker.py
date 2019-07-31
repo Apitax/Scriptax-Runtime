b71010e5e9bd79a32b95a8f269dea667f9011010
Shawn Clake <shawn.clake@gmail.com>
2019-07-31 09:15:56 -0600

    Added a string based invoker

--- scriptax_runtime/invoker.py
+++ scriptax_runtime/invoker.py
@@ -64,3 +64,35 @@ def execute(file: str, debug: bool = False) -> Tuple[BlockStatus, AhVisitor, flo
     block_status, ahvisitor = customizable_parser(read_file(file), file=file, options=Options(debug=debug))
     total_time = time.process_time() - start_time
     return block_status, ahvisitor, total_time
+
+
+def execute_string(code: str, debug: bool = False) -> Tuple[BlockStatus, AhVisitor, float]:
+    Drivers.add("scriptax", ScriptaxDriver(path="~/"))
+    LoadedDrivers.load("scriptax")
+
+    drivers_path = Path(Path(__file__).resolve().parents[1]).joinpath('drivers.json')
+    packages = list(drivers_path)
+    for key, value in packages.items():
+        drivers = value['drivers']
+
+        # Imports default driver
+        try:
+            module = importlib.import_module(key + '.drivers.driver')
+            Drivers.add(key, module.driver)
+            LoadedDrivers.load(key)
+        except:
+            print("Unable to load default driver inside of package: " + key)
+
+        # Imports other drivers
+        for driver in drivers:
+            try:
+                module = importlib.import_module(key + '.drivers.' + driver)
+                Drivers.add(key + "_" + driver, module.driver)
+                LoadedDrivers.load(key + "_" + driver)
+            except:
+                print("Unable to load driver `" + driver + "` inside of package: " + key)
+
+    start_time = time.process_time()
+    block_status, ahvisitor = customizable_parser(read_string(code), options=Options(debug=debug))
+    total_time = time.process_time() - start_time
+    return block_status, ahvisitor, total_time
